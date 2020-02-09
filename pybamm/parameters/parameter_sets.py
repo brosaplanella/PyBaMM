6be06b77670004eb01be2ee52b0885ce263445df
Ferran Brosa Planella <Ferran.Brosa-Planella@warwick.ac.uk>
2020-02-09 18:52:54 +0000

    #573 included Chen2020 as a PyBaMM parameter set

--- pybamm/parameters/parameter_sets.py
+++ pybamm/parameters/parameter_sets.py
@@ -16,6 +16,7 @@ NCA_Kim2011
     (2011). Multi-domain modeling of lithium-ion batteries encompassing
     multi-physics in varied length scales. Journal of The Electrochemical
     Society, 158(8), A955-A969.
+
 Chen2020
     Chang-Hui Chen, Ferran Brosa Planella, Kieran O’Regan, Dominika Gastol, W. Dhammika Widanage, and Emma Kendrick. "The development of accurate experimental techniques for parameterization of multi-scale lithium-ion battery models." In preparation (2020).
 
