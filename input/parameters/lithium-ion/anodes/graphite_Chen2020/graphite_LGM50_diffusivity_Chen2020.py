da7d436b6a707a0416ce488efd36dad8004f1090
Ferran Brosa Planella <Ferran.Brosa-Planella@warwick.ac.uk>
2020-02-24 15:51:16 +0000

    update parameters

--- input/parameters/lithium-ion/anodes/graphite_Chen2020/graphite_LGM50_diffusivity_Chen2020.py
+++ input/parameters/lithium-ion/anodes/graphite_Chen2020/graphite_LGM50_diffusivity_Chen2020.py
@@ -1,13 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 from pybamm import exp
-=======
-import autograd.numpy as np
-
->>>>>>> #573 update final LGM50 parameters
-=======
-from pybamm import exp
->>>>>>> #573 setup test as experiment, still fails to converge
 
 def graphite_LGM50_diffusivity_Chen2020(sto, T, T_inf, E_D_s, R_g):
     """
@@ -34,23 +25,7 @@ def graphite_LGM50_diffusivity_Chen2020(sto, T, T_inf, E_D_s, R_g):
          Solid diffusivity
    """
 
-<<<<<<< HEAD
-<<<<<<< HEAD
     D_ref = 3.3e-14
     arrhenius = exp(E_D_s / R_g * (1 / T_inf - 1 / T))
 
     return D_ref * arrhenius
-=======
-    D_ref = 6e-15 * 2.2  # * 2.2 * 5.5 * 10.5
-    arrhenius = np.exp(E_D_s / R_g * (1 / T_inf - 1 / T))
-
-    correct_shape = 0 * sto
-
-    return D_ref * arrhenius + correct_shape
->>>>>>> #573 update final LGM50 parameters
-=======
-    D_ref = 3.3e-14
-    arrhenius = exp(E_D_s / R_g * (1 / T_inf - 1 / T))
-
-    return D_ref * arrhenius
->>>>>>> #573 setup test as experiment, still fails to converge
